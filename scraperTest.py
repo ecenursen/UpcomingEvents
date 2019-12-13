d3ac4d0d92212c4a014a5e977168133320f78f20
goktugbasaran <gokktugbasaran@gmail.com>
2019-12-13 17:16:55 +0300

    Test repetative heroku crawling

--- scraperTest.py
+++ scraperTest.py
@@ -1,35 +1,39 @@
 from scrapy import Spider
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess,CrawlerRunner
 from scrapy.selector import Selector
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
 import time
+global crawlerProcessStarted
+crawlerProcessStarted = False
+
 
 class ScraperTest(Spider):
-    name = "ScraperTest"
-    allowed_domains = ["biletinial.com"]
-    start_urls = ["https://www.biletinial.com/muzik"]
+	name = "ScraperTest"
+	allowed_domains = ["biletinial.com"]
+	start_urls = ["https://www.biletinial.com/muzik"]
 
-    def parse(self, response):
-        events = Selector(response).xpath(
-            '/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div')
-        for event in events:
-            print(event.xpath(
-                'a/div[@class="event-title"]/text()').extract()[0])
+	def parse(self, response):
+		events = Selector(response).xpath(
+			'/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div')
+		for event in events:
+			print(event.xpath(
+				'a/div[@class="event-title"]/text()').extract()[0])
 
+def sleep(_,duration = 30):
+	print(f'sleeping for: {duration}')
+	time.sleep(duration)
 
-def scrape(name):
-    print("Crawling process {} is starting...".format(name))
-    process = CrawlerProcess()
-    process.crawl(ScraperTest)
-    print("Crawler attached...")
-    process.start()
-    print("Crawler finished...")
+def crawl(runner):
+	d = runner.crawl(ScraperTest)
+	d.addBoth(sleep)
+	d.addBoth(lambda _: crawl(runner))
+	return d
 
+def loop_crawl():
+	runner = CrawlerRunner(get_project_settings())
+	crawl(runner)
+	reactor.run()
 
 if __name__ == "__main__":
-    print("Regular scraping is beginning...")
-    start = time.time()
-    counter = 0
-    while(True):
-        if(time.time() - start > 30):
-            scrape(counter)
-            start = time.time()
+	loop_crawl()
