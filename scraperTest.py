4b166fdc5b8e52427f6feeb37cdff6938457e610
goktugbasaran <gokktugbasaran@gmail.com>
2019-12-21 23:17:06 +0300

    Revert "Add date parser"
    This reverts commit 3c0baae74bd5474a6a39261974834d43a488b1aa.

--- scraperTest.py
+++ scraperTest.py
@@ -25,8 +25,8 @@ class ScraperItem(Item):
 	description = Field()
 
 
-class BiletiniAlScraper(Spider):
-	name = "BiletiniAlScraper"
+class BiletiniAl(Spider):
+	name = "BiletiniAl"
 	allowed_domains = ["biletinial.com"]
 	start_urls = ["https://www.biletinial.com/muzik",
 				  "https://biletinial.com/egitim", "https://biletinial.com/tiyatro",
@@ -35,65 +35,39 @@ class BiletiniAlScraper(Spider):
 	def parse(self, response):
 		events = Selector(response).xpath(
 			'/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div')
-
+		print("BiletiniAl runs...")
 		for event in events:
-			'''
-			global eventCounter
-			print("Scraping:", eventCounter, "Biletinial")
-			eventCounter += 1
-			'''
-			date = ' '.join(s.strip().translate(trans_table)
+			item = ScraperItem()
+			item['name'] = event.xpath(
+				'a/div[@class="event-title"]/text()').extract()[0]
+			item['url'] = "https://biletinial.com" + \
+				event.xpath('a/@href').extract()[0]
+			item['location'] = event.xpath(
+				'a/div[@class="place"]/span/text()').extract()[0]
+			item['city'] = event.xpath(
+				'a/div[@class="place"]/span/b/text()').extract()[0]
+			item['image'] = event.xpath(
+				'a/img[@class="moimag"]/@data-path').extract()[0]
+			item['date'] = ' '.join(s.strip().translate(trans_table)
 									for s in event.xpath('a/ul/li/span/text()').extract())
-			dates = self.parseDate(date)
-			for date in dates:
-				item = ScraperItem()
-				item['name'] = event.xpath(
-					'a/div[@class="event-title"]/text()').extract()[0]
-				item['date'] = date
-				item['url'] = "https://biletinial.com" + \
-					event.xpath('a/@href').extract()[0]
-				
-				item['location'] = event.xpath(
-					'a/div[@class="place"]/span/text()').extract()[0]
-				item['city'] = event.xpath(
-					'a/div[@class="place"]/span/b/text()').extract()[0]
-				item['image'] = event.xpath(
-					'a/img[@class="moimag"]/@data-path').extract()[0]
-				item['type_'] = event.xpath(
-					'a/div[@class="category"]/text()').extract()[0]
-				
-				request = Request(
-					url=item['url'], callback=self.get_description, meta={'item': item})
-				yield request
-	
+			item['type_'] = event.xpath(
+				'a/div[@class="category"]/text()').extract()[0]
+			request = Request(
+				url=item['url'], callback=self.get_description, meta={'item': item})
+			yield request
+
 	def get_description(self, response):
 		page = Selector(response)
 		item = ScraperItem(response.meta["item"])
 		item['description'] = ' '.join(s.strip().translate(
 			trans_table) for s in page.xpath('//*[@id="movie-detail"]//p/text()').extract())
-		print(add_event(item['name'],item['city'],item['location'],item['date'],item['type_'],item['url'],item['image']))
-	
-	def parseDate(self, date):
-		dateStr = date.replace(" ","").split("-")  
+		# TODO:: CREATE ITEM API CONNECTION
+		print(add_scrapped(json.dumps(dict(item), ensure_ascii=False)))
+		yield item
 
-		eventMonth = months.index(dateStr[0])+1
-		days = dateStr[1:]    
-		
-		dates = []
-		currentMonth = datetime.datetime.today().month
-		currentYear = datetime.datetime.today().year
-		for day in days:
-			eventYear = currentYear+1 if eventMonth-currentMonth<0 else currentYear
-			if(eventMonth < 10):
-				month = "0{}".format(eventMonth)
-			else:
-				month = "{}".format(eventMonth)
-			dateString="{}-{}-{}".format(eventYear,month,day)
-			dates.append(dateString)
-		return dates
 
-class CRRScraper(Spider):
-	name = "CRRScraper"
+class CemalResitRey(Spider):
+	name = "CemalResitRey"
 	allowed_domains = ["crrkonsersalonu.ibb.istanbul"]
 	start_date = time.strftime("%d.%m.%y")
 	end_date = (datetime.datetime.now() + datetime.timedelta(100)
@@ -101,18 +75,18 @@ class CRRScraper(Spider):
 	end_date = end_date[-2:] + "." + end_date[5:-3] + "." + end_date[:4]
 	start_urls = [
 		"https://crrkonsersalonu.ibb.istanbul/Home/Events?CRRLang=tr-TR&startDate={}&endDate={}&dateRange=0&categoryId=0".format(start_date, end_date)]
-		
+
 	def parse(self, response):
 
 		events = Selector(response).xpath(
 			'/html/body/div[2]/div/div[3]/div/div[3]/div')
+		print("CemalResitRey runs...")
 		for event in events:
 			item = ScraperItem()
+			item['date'] = event.xpath(
+				'div[@class="dateColumn"]/span/text()').extract()[0]
 			item['name'] = event.xpath(
 				'div[@class="detailsColumn"]/div/div/h4/text()').extract()[0]
-			date = self.parseDate(event.xpath(
-				'div[@class="dateColumn"]/span/text()').extract()[0])
-			item['date'] = date
 			item['type_'] = 'Konser'
 			item['image'] = event.xpath(
 				'div[@class="detailsColumn"]/div/img/@src').extract()[0]
@@ -121,23 +95,9 @@ class CRRScraper(Spider):
 			item['location'] = 'Cemal Reşit Rey'
 			item['city'] = 'İstanbul'
 			item['description'] = ""
-			print(add_event(item['name'],item['city'],item['location'],item['date'],item['type_'],item['url'],item['image']))
-			
+			print(add_scrapped(json.dumps(dict(item), ensure_ascii=False)))
+			yield item
 
-	def parseDate(self,date):
-		date=date.split(" ")
-		eventMonth = months.index(date[1])+1
-		day = date[0]
-		
-		currentMonth = datetime.datetime.today().month
-		currentYear = datetime.datetime.today().year
-		eventYear = currentYear+1 if eventMonth-currentMonth<0 else currentYear
-		if(eventMonth < 10):
-			month = "0{}".format(eventMonth)
-		else:
-			month = "{}".format(eventMonth)
-		dateString="{}-{}-{}".format(eventYear,month,day)
-		return dateString
 
 class BaskaSinema(Spider):
 
@@ -148,17 +108,11 @@ class BaskaSinema(Spider):
 	def parse(self, response):
 		events = Selector(response).xpath(
 			'/html/body/div[2]/div[2]/div/div/div[2]/div[2]/div')
-
+		print("BaskaSinema runs...")
 		for event in events:
-			'''
-			global eventCounter
-			print("Scraping:", eventCounter, "baskasinema")
-			eventCounter += 1
-			'''
 			item = ScraperItem()
-			date = event.xpath(
+			item['date'] = event.xpath(
 				'div[@class="movie_info_box"]/div[@class="movie_info"]/strong/text()').extract()[0]
-			item['date'] = self.parseDate(date)
 			item['name'] = event.xpath(
 				'div[@class="movie_info_box"]/h2/span/text()').extract()[0]
 			item['type_'] = "Sinema"
@@ -168,19 +122,8 @@ class BaskaSinema(Spider):
 			item['city'] = ""
 			item['description'] = ' '.join(s.strip().translate(trans_table)
 										   for s in event.xpath('div[@class="movie_info_box"]/div[@class="movie_info"]//text()').extract())
-			print(add_event(item['name'],item['city'],item['location'],item['date'],item['type_'],item['url'],item['image']))
-
-	def parseDate(self, date):
-		date=date.split(" ")
-		eventMonth = months.index(date[1])+1
-		if(eventMonth < 10):
-			month = "0{}".format(eventMonth)
-		else:
-			month = "{}".format(eventMonth)
-		day = date[0]
-		eventYear = date[2]
-		dateString="{}-{}-{}".format(eventYear,month,day)
-		return dateString
+			print(add_scrapped(json.dumps(dict(item), ensure_ascii=False)))
+			yield item
 
 
 def sleep(_, duration=60):
